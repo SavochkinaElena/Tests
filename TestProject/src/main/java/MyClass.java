f03ec0e8bdeae7e544fae1dad131fc80caf5f238
SavochkinaElena <s.elena.alexandrovna@gmail.com>
2020-07-19 22:07:13 +0300

    Создан проект для IDEA

--- TestProject/src/main/java/MyClass.java
+++ TestProject/src/main/java/MyClass.java
@@ -1,8 +1,8 @@
 public class MyClass {
 
-public static void main(String[] args) {
+    public static void main(String[] args) {
 
-System.out.println("Hello");
+        System.out.println("Hello");
 
-}
+    }
 }
