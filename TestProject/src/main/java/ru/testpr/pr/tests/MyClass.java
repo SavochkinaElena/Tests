ba81e6d7c6230718ea8ea4d3bbe5e0a72f8ba98e
SavochkinaElena <s.elena.alexandrovna@gmail.com>
2020-07-19 22:37:49 +0300

    Добавлена функция

--- TestProject/src/main/java/ru/testpr/pr/tests/MyClass.java
+++ TestProject/src/main/java/ru/testpr/pr/tests/MyClass.java
@@ -3,8 +3,12 @@ package ru.testpr.pr.tests;
 public class MyClass {
 
     public static void main(String[] args) {
+       hello("world");
+       double l = 5;
+        System.out.println(l);
+    }
 
-        System.out.println("Hello");
-
+    public static void hello(String some) {
+        System.out.println("Hello " + some);
     }
 }
