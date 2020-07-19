83495bb3acba9149c904f47fb50577e660b836d1
SavochkinaElena <s.elena.alexandrovna@gmail.com>
2020-07-19 22:51:51 +0300

    Добавлены методы в классы

--- TestProject/src/main/java/ru/testpr/pr/tests/MyClass.java
+++ TestProject/src/main/java/ru/testpr/pr/tests/MyClass.java
@@ -4,16 +4,8 @@ public class MyClass {
 
     public static void main(String[] args) {
         Square s = new Square(5);
-        System.out.println(area(s));
+        System.out.println(s.area());
         Rectangle r = new Rectangle(5, 6);
-        System.out.println(area(r));
-    }
-
-    public static double area(Square s) {
-       return s.l * s.l;
-    }
-
-    public static double area(Rectangle r) {
-       return r.a * r.b;
+        System.out.println(r.area());
     }
 }
--- TestProject/src/main/java/ru/testpr/pr/tests/Rectangle.java
+++ TestProject/src/main/java/ru/testpr/pr/tests/Rectangle.java
@@ -9,4 +9,9 @@ public class Rectangle {
         this.b = b;
     }
 
+    public double area() {
+        return this.a * this.b;
+    }
+
+
 }
